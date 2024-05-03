65c6aaf9a37231f947845dc28440803558e55d1e
EL HATHOUT LINA <lunarowki@gmail.com>
2024-05-03 12:48:45 +0200

    first commit

---
+++ Lina.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var rectangle1 = document.querySelector(".rectangle1");
+    var rect1 = document.querySelector(".rect-1");
+    rectangle1.addEventListener("mouseenter", function() {
+        rect1.classList.add("show");
+    });
+    rectangle1.addEventListener("mouseleave", function() {
+        rect1.classList.remove("show");
+    });
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var rectangle2 = document.querySelector(".rectangle2");
+    var rect2 = document.querySelector(".rect-2");
+    rectangle2.addEventListener("mouseenter", function() {
+        rect2.classList.add("show");
+    });
+    rectangle2.addEventListener("mouseleave", function() {
+        rect2.classList.remove("show");
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var rectangle3 = document.querySelector(".rectangle3");
+    var rect3 = document.querySelector(".rect-3");
+    rectangle3.addEventListener("mouseenter", function() {
+        rect3.classList.add("show");
+    });
+    rectangle3.addEventListener("mouseleave", function() {
+        rect3.classList.remove("show");
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var rectangle4 = document.querySelector(".rectangle4");
+    var rect4 = document.querySelector(".rect-4");
+    rectangle4.addEventListener("mouseenter", function() {
+        rect4.classList.add("show");
+    });
+    rectangle4.addEventListener("mouseleave", function() {
+        rect4.classList.remove("show");
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var rectangle5 = document.querySelector(".rectangle5");
+    var rect5 = document.querySelector(".rect-5");
+    rectangle5.addEventListener("mouseenter", function() {
+        rect5.classList.add("show");
+    });
+    rectangle5.addEventListener("mouseleave", function() {
+        rect5.classList.remove("show");
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var rectangle6 = document.querySelector(".rectangle6");
+    var rect6 = document.querySelector(".rect-6");
+    rectangle6.addEventListener("mouseenter", function() {
+        rect6.classList.add("show");
+    });
+    rectangle6.addEventListener("mouseleave", function() {
+        rect6.classList.remove("show");
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var rectangle7 = document.querySelector(".rectangle7");
+    var rect7 = document.querySelector(".rect-7");
+    rectangle7.addEventListener("mouseenter", function() {
+        rect7.classList.add("show");
+    });
+    rectangle7.addEventListener("mouseleave", function() {
+        rect7.classList.remove("show");
+    });
+});
